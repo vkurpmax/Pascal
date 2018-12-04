a0adb661bffb353ee974ee1563b18e68ededea41
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-04 21:03:11 +0300

    Create 3struktur_perulangan.pas

---
+++ MyProgram/WikipediaID/3struktur_perulangan.pas
@@ -0,0 +1,12 @@
+var
+  total,a,b:integer;
+begin
+  writeln('Masukkan nilai a');
+  readln(a);
+  writeln('Masukkan nilai b');
+  readln(b);
+  writeln('hasil’,a,’pengurang‘,b,’adalah’,total);
+  total:= a - b;
+  writeln(total);
+  readln;
+end.
