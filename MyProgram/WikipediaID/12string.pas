7b471c36398eb20bed51560f7e62918f88167aeb
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-04 21:12:01 +0300

    Create 12string.pas

---
+++ MyProgram/WikipediaID/12string.pas
@@ -0,0 +1,15 @@
+program string;
+var
+  p,l,t,v:integer;
+
+begin
+  writeln('panjang');
+  readln(p);
+  writeln('lebar');
+  readln(l);
+  writeln('tinggi');
+  readln(t);
+  v := p * l * t;
+  writeln(v);
+  readln;
+end.
