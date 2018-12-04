bfb4478f905e735407feb6388181bd5d74badbd1
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-04 19:53:28 +0300

    Create note.cmd

---
+++ Android/Learn Pascal Programming/note.cmd
@@ -0,0 +1,6 @@
+One last note on errors: there are 2 main types of errors, namely - Runtime Errors and Compilation Errors.
+
+Runtime errors are those which occur unexpectedly during the execution of the program,
+whereas a Compilation error is one which is detected during the compilation process.
+Note that a decimal number is also considered as a wrong input;
+a decimal number must not be input, since it is a real number (more on this later).
