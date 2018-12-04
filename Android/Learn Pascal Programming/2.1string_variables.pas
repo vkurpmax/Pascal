6e272c727d913a2a6895061e5285dd13385b38aa
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-04 20:00:09 +0300

    Create 2.1string_variables.pas

---
+++ Android/Learn Pascal Programming/2.1string_variables.pas
@@ -0,0 +1,14 @@
+program Lesson2_Program1;
+var
+  name,surname:string;
+
+begin
+  write('Enter your name:');
+  readln(name);
+  write('Enter your surname:');
+  readln(surname);
+  writeln;        {new line}
+  writeln;        {new line}
+  writeln('Your full name is: ',name,' ',surname);
+  readln;
+end.  
