68e97075c308af447ffde5bfaa720f53266b07d2
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-04 19:55:03 +0300

    Create 1.3penjumlahan.pas

---
+++ Android/Learn Pascal Programming/1.3penjumlahan.pas
@@ -0,0 +1,13 @@
+program Lesson1_Program3;
+var
+  Num1,Num2,Sum:integer;
+
+begin                   {no semicolon}
+  write('Input number 1:');
+  readln(num1);
+  writeln('Input number 2:');
+  readln(num2);
+  sum:=num1+num2;       {addition}
+  writeln(sum);
+  readln;
+end.
