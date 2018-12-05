7f72620b5357ea878e5be61148fc0231bde4b692
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-06 01:53:47 +0300

    Create ClrScr.pas

---
+++ Freepascal.org/ClrScr.pas
@@ -0,0 +1,12 @@
+Program Example8;
+uses Crt;
+
+{ Program to demonstrate the ClrScr function. }
+
+begin
+  Writeln('Press any key to clear the screen');
+  ReadKey;
+  ClrScr;
+  Writeln('Have fun with the cleared screen');
+  readln;
+end.
