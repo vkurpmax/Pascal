84f0c41e551ed2450779375d5da56f426b00260f
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-06 03:15:46 +0300

    Create 3.2.2GotoXY.pas

---
+++ Android/Learn Pascal Programming/3.2.2GotoXY.pas
@@ -0,0 +1,9 @@
+program Project1;
+uses crt;
+
+begin
+  gotoxy(10,10);
+  writeln('The position is 10 pixels from the left of the screen, and ten pixels');
+  writeln('from the top of the screen.');
+  readln;
+end. 
