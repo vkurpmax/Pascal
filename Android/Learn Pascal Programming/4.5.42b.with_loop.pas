a8bc32adfd0903570cb15c117a8f666c80c0706f
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-06 19:18:59 +0300

    Create 4.5.42b.with_loop.pas

---
+++ Android/Learn Pascal Programming/4.5.42b.with_loop.pas
@@ -0,0 +1,9 @@
+program lesson4_Program2b;
+uses crt;
+var counter:integer; {loop counter declared as integer}
+
+begin
+  for counter:=1 to 7 do {it's easy and fast!}
+    writeln('for loop');
+  readln;
+end.
