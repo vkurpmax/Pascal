d337dbb2909683fd467849235504936657ca4bf6
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-06 19:14:33 +0300

    Create 4.5.42a.for_loop.pas

---
+++ Android/Learn Pascal Programming/4.5.42a.for_loop.pas
@@ -0,0 +1,13 @@
+program lesson4_program2a;
+uses crt;
+
+begin
+  writeln('for loop'); {somewhat tedious writing all this!!!}
+  writeln('for loop');
+  writeln('for loop');
+  writeln('for loop');
+  writeln('for loop');
+  writeln('for loop');
+  writeln('for loop');
+  readln;
+end. 
