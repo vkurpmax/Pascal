4b5518e6435f3f27c502a7ecd2124ffdfc4abbc2
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-06 20:10:43 +0300

    Create 4.5.43a.gotoxy_with_loop.pas

---
+++ Android/Learn Pascal Programming/4.5.43a.gotoxy_with_loop.pas
@@ -0,0 +1,35 @@
+program lesson4_Program3a;
+uses crt;
+
+begin
+  gotoxy(25,5);writeln('+');
+  gotoxy(25,6);writeln('I');
+  gotoxy(25,7);writeln('I');
+  gotoxy(25,8);writeln('I');
+  gotoxy(25,9);writeln('I');
+  gotoxy(25,10);writeln('I');
+  gotoxy(25,11);writeln('+');
+
+  gotoxy(26,11);writeln('-');
+  gotoxy(27,11);writeln('-');
+  gotoxy(28,11);writeln('-');
+  gotoxy(29,11);writeln('-');
+  gotoxy(30,11);writeln('-');
+  gotoxy(31,11);writeln('-');
+
+  gotoxy(32,5);writeln('+');
+  gotoxy(32,6);writeln('I');
+  gotoxy(32,7);writeln('I');
+  gotoxy(32,8);writeln('I');
+  gotoxy(32,9);writeln('I');
+  gotoxy(32,10);writeln('I');
+  gotoxy(32,11);writeln('+');
+
+  gotoxy(26,5);writeln('-');
+  gotoxy(27,5);writeln('-');
+  gotoxy(28,5);writeln('-');
+  gotoxy(29,5);writeln('-');
+  gotoxy(30,5);writeln('-');
+  gotoxy(31,5);writeln('-'); {Finally!!! Phew!}
+  Readln; {wait for user to read}
+end.
