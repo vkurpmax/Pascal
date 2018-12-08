98c435282d342dafe060a3aa18df8b01c741e7ea
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-08 22:42:48 +0300

    Create 10.2.8_Procedure_Str.pas

---
+++ Android/Learn Pascal Programming/10.2.8_Procedure_Str.pas
@@ -0,0 +1,16 @@
+program Project1;
+var
+  S:string;
+  i:real;
+
+begin
+  i:= -0.563;
+  str(i,s);
+  write(s);
+  readln;
+end.
+
+{Procedure Str(Val:Integer/LonInt/Real; var s:string);
+Description
+Converts an integer or a decimal value to a string. The value parameter Val is
+converted into a string and passed through the variable parameter S.}
