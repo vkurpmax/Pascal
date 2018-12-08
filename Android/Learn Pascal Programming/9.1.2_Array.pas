aa6c96021f8369294dd48277d4722c70d3d184b7
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-08 16:33:30 +0300

    Create 9.1.2_Array.pas

---
+++ Android/Learn Pascal Programming/9.1.2_Array.pas
@@ -0,0 +1,13 @@
+program Project1;
+var
+  i:integer;
+  myIntArray:array[1..20] of integer;
+  myBoolArray:array[1..20] of boolean;
+
+begin
+  for i:=1 to length(myIntArray) do
+  begin
+    myintarray[i]:=0;
+    myboolarray[i]:=false;
+  end;
+end.
