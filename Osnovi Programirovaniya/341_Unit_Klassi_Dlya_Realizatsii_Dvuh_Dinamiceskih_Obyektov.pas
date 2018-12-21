496f5c2a403c720bfc5d26985cf393003eda98b9
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2018-12-22 00:43:19 +0300

    Update 341_Unit_Klassi_Dlya_Realizatsii_Dvuh_Dinamiceskih_Obyektov.pas

--- Osnovi Programirovaniya/341_Unit_Klassi_Dlya_Realizatsii_Dvuh_Dinamiceskih_Obyektov.pas
+++ Osnovi Programirovaniya/341_Unit_Klassi_Dlya_Realizatsii_Dvuh_Dinamiceskih_Obyektov.pas
@@ -18,22 +18,22 @@ interface
 
 
 implementation
-Function TRoomD.Square;  {тело метода определения площади}
+Function TRoomD.Square:real;  {тело метода определения площади}
 begin
   Square:=length*width;
 end;
-Constructor TRoomD.Init;  {тело конструктора}
+Constructor TRoomD.Init(l,w:real);  {тело конструктора}
 begin
   length:=l;
   width:=w;
 end;
-constructor TVRoomD.Init;
+constructor TVRoomD.Init(l,w,h:real);
 begin
   inherited Init(l,w);  {инициализирует поля базового класса}
   height:=h;  {инициализируем собственное поле класса}
 end;
-Function TVRoomD.Square;
+Function TVRoomD.Square:real;
 begin
-  Square:=inherited Square+2*height(length+width);
+  Square:=inherited Square+2*height*(length+width);
 end;
 end.
